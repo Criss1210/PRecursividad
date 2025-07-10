e6d0231015403a48fe2dd8b4f9dd396c55d22158
Cristal <cristalonedys@gmail.com>
2025-07-10 19:21:00 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace PracticaRecursividadSumaDigitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce un número natural: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            int sumaRecursiva = SumarDigitosRecursivo(numero);
+            int sumaIterativa = SumarDigitosIterativo(numero);
+
+            Console.WriteLine($"\nSuma de dígitos (recursiva): {sumaRecursiva}");
+            Console.WriteLine($"Suma de dígitos (iterativa): {sumaIterativa}");
+        }
+
+        // Método recursivo
+        static int SumarDigitosRecursivo(int n)
+        {
+            if (n < 10)
+                return n;
+            else
+                return (n % 10) + SumarDigitosRecursivo(n / 10);
+        }
+
+        // Método iterativo
+        static int SumarDigitosIterativo(int n)
+        {
+            int suma = 0;
+            while (n > 0)
+            {
+                suma += n % 10;
+                n /= 10;
+            }
+            return suma;
+        }
+    }
+}
